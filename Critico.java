b56e59b2295a2a233c0d3501ec6452bd3e3fc18b
PH102003 <100202317+PH102003@users.noreply.github.com>
2023-05-15 22:00:46 -0300

    Update Critico.java

--- Critico.java
+++ Critico.java
@@ -1,7 +1,6 @@
 public class Critico extends Usuario {
     public String origem;
     public double nota;
-    public Compra bilhete0Compra  = new Compra();
     public Critico(String user, String CPF, String senha, int idade, char sexo, String email, String nomeDoCartao,
     int numeroDoCartao, int idDoCartao,Compra compra){
         super(user, CPF, senha, idade, sexo, email, nomeDoCartao, numeroDoCartao, idDoCartao, compra);
